eee458b273b3dd1e290292f50f63fc708aba1665
Jordan Hensley <Jordy@jordans-macbook-pro.local>
2017-05-16 15:42:48 -0400

    Initial Commit

---
+++ Lab1A/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1A
+//
+//  Created by Jordan Hensley on 5/16/17.
+//  Copyright © 2017 Jordan Hensley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
